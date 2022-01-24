d5b3747f732f83a929519fa57be7fb3a0ed59124
ThomasScialom <t.scialom@gmail.com>
2022-01-24 18:02:35 +0100

    Update metric_scorer.py

--- t0_continual_learning/metric_scorer.py
+++ t0_continual_learning/metric_scorer.py
@@ -298,7 +298,7 @@ class MetricScorer():
 def whatMetric(dataset_name, prompt_name, force_nlg='bleu', force_nlu='accuracy'):
   
   nlg_datasets = {'haiku', 'eli5', 'wiki_auto', 'gigaword', 'covid_qa_deepset'}
-  nlu_datasets = { 'rte', 'copa', 'wic', 'winogrande', 'hellaswag', 'anli', 'cb', 'wsc', 'story_cloze', 'covidfact','rank_summary_OpenAI'}
+  nlu_datasets = { 'rte', 'copa', 'wic', 'winogrande', 'hellaswag', 'anli', 'cb', 'wsc', 'story_cloze', 'covidfact','rank_summary'}
 
   if 'constrain' in prompt_name:
     if 'constrain_start' in prompt_name:
